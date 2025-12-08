02a38a73f1421915d09955cebb9c9078954c9915
Subfly <alitahasubfly@gmail.com>
2025-12-08 21:34:10 +0100

    Add splash screen for Android

--- Compose/YABA/composeApp/src/androidMain/kotlin/dev/subfly/yaba/MainActivity.kt
+++ Compose/YABA/composeApp/src/androidMain/kotlin/dev/subfly/yaba/MainActivity.kt
@@ -6,11 +6,22 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : ComponentActivity() {
+
+    /**
+     * Set to true when you need to hold the splash screen while preparing app state.
+     */
+    private var keepSplashOnScreen: Boolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
-        enableEdgeToEdge()
+        val splashScreen = installSplashScreen()
         super.onCreate(savedInstanceState)
+
+        splashScreen.setKeepOnScreenCondition { keepSplashOnScreen }
+
+        enableEdgeToEdge()
 
         setContent {
             App()
